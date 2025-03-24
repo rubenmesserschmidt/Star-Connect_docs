e308797640baca865ea76b9d60b4630e452b09b1
Ruben Messerschmidt <ruben.messerschmidt@outlook.com>
2025-03-24 10:03:29 +0100

    update 1.4.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = "Star Connect"
 copyright = "2024, Ruben Messerschmidt"
 author = "Ruben Messerschmidt"
 
-release = "1.4.1"
-version = "1.4.1"
+release = "1.4.2"
+version = release
 
 # -- General configuration
 
